819a4544b4e3aaa232d459573c71b7ff15c75c1a
Sanrose <sanrosebhetuwal@gmail.com>
2026-06-29 15:53:24 +0545

    node js

--- server.js
+++ server.js
@@ -1,42 +1,29 @@
 const express = require('express');
-
-const professionals = require('./data/professional');
 const app = express();
-app.use(express.json())
-const PORT = 3000;
+const port = 3000;
 
-//naming convention for api endpoints is to use plural nouns
-app.get('/api/professionals', (req, res) => {
-  res.status(200).json(professionals);
-});
-
-app.get('/api/professionals/:id', (req, res) => {
-    const professionalId = parseInt(req.params.id,10);
-    const professional = professionals.find((p) => p.id === professionalId);
-
-    if (!professional){
-        return res.status(404).json({message: 'Professional not found'});
-    }
-    res.status(200).json(professional);
-});
-
-app.post('/api/professionals', (req, res) => {
-    const {name, category} = req.body;
-
-    if (!name || !category){
-        return res.status(400).json({message: 'Name and category are required'});
-    }
-
-    const newProfessional = {
-        id: professionals.length + 1,
-        name,
-        category
-    }
-
-    professionals.push(newProfessional);
-    res.status(201).json({message: 'Professionals created successfully', category, name});
+//connecting to DB
+const mongoose = require(mongoose);
+mongoose.connect("mongodb://localhost:27017/professionalsDB",{
+  userNewUrlParser: true,
+  useUnifiedTopology:true,
 })
+.then(() => console.log("connected to MongoDB"))
+.catch((err) => console.error("Could not Connect to Mongo DB",err2))
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-}); 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// import professionals data
+const professionals = require('./data/professional.js');
+
+//importing routes
+const professionalRoutes = require("./routes/professionalroutes")
+
+//using the app
+app.use("/api/professionals", professionalRoutes)
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
